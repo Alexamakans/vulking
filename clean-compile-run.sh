de6c8216da3cf54268988932ad8935610838b77c
Alexander Fougner <alexander.fougner@iver.se>
2025-05-06 11:36:43 +0200

    add install dependencies

--- clean-compile-run.sh
+++ clean-compile-run.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+echo "Installing dependencies"
+sudo dnf install vulkan vulkan-headers vulkan-loader-devel glfw-devel glm-devel vulkan-validation-layers glslc
+
 mkdir -p assets/shaders
 pushd assets/shaders
 echo "Removing compiled shaders"
-# sudo dnf install glslc
 echo "Compiling vertex shaders"
 find ./ -type f -iname "*.vert" -exec sh -c 'glslc "$1" -o "$1.spv"' _ {} \;
 echo "Compiling fragment shaders"
