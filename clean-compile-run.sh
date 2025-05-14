a171b956987c0bb0b643026f5d6c4faba9c3fe4e
Alexamakans <fougner.alexander@gmail.com>
2025-05-14 21:24:47 +0200

    wow, i can write tests now

--- clean-compile-run.sh
+++ clean-compile-run.sh
@@ -28,4 +28,5 @@ if [ -L ./compile_commands.json ]; then
 fi
 ln -s ./build/compile_commands.json ./compile_commands.json
 
+./build/tests --durations yes --allow-running-no-tests "~[never_run]"
 ./build/vulking
